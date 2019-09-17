01a1d43be036bb314071f5ccbdd85b8f3e7d1ead
Eddy Tyannikov <etyannikov@gmail.com>
2019-09-17 23:08:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  elsif array.size == 2 
+    return array.join(" and ")
+  else array.size == 3
+    last = array.pop
+    array << "and "
+    string = array.join(", ")
+    string << last
+end
 end
